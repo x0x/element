a306662f69a2e0ab31f04aa79d3fbf753932d8ed
x0x <priyanshu@tutanota.de>
2022-04-07 19:46:15 +0530

    Initial code upload

---
+++ include/Camera.h
@@ -0,0 +1,36 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+
+#include "utils.h"
+
+class Camera
+{
+public:
+	glm::vec3 eye;
+	glm::vec3 center;
+	glm::vec3 up;
+	float fov;
+
+	const int WIDTH;
+	const int HEIGHT;
+
+	Camera(
+		const glm::vec3 eye,
+		const glm::vec3 center,
+		const glm::vec3 up,
+		const float fov,
+		const int width,
+		const int height);
+	~Camera(){};
+
+	Ray castRay(
+		const int pixel_x, // [0, WIDTH_ - 1]
+		const int pixel_y, // [0, HEIGHT_ - 1]
+		const float parameter_x, // [-0.5, 0.5]
+		const float parameter_y); // [-0.5, 0.5]
+};
+
+#endif // CAMERA_H
